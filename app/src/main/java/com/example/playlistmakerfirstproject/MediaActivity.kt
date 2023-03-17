90688ea491fb767dde12ee2b10b4179ef838a687
Morozov0208Saveliy <savelmor@yandex.ru>
2023-03-17 18:15:08 +0100

    Sprint №9 первая попытка

--- app/src/main/java/com/example/playlistmakerfirstproject/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmakerfirstproject/MediaActivity.kt
@@ -3,7 +3,7 @@ package com.example.playlistmakerfirstproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MediaActivity : AppCompatActivity() {
+public class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
