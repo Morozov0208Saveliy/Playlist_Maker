a497e837f7ce684dd69b65ac5895a1aa7ff6070c
Morozov0208Saveliy <savelmor@yandex.ru>
2023-05-23 20:39:16 +0200

    Sprint №12(1)

--- app/src/main/java/com/example/playlistmakerfirstproject/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakerfirstproject/SearchActivity.kt
@@ -40,18 +40,19 @@ class SearchActivity : AppCompatActivity(), SharedPreferences.OnSharedPreference
     private lateinit var placeholderMessage: TextView
 
     private var lastRequest: String = ""
-    var inputSaveText: String = ""
+    private var inputSaveText: String = ""
     private val itunesBaseUrl = "https://itunes.apple.com"
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(itunesBaseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private val retrofit =
+        Retrofit.Builder().baseUrl(itunesBaseUrl).addConverterFactory(GsonConverterFactory.create())
+            .build()
     private val itunesService = retrofit.create(ApiForItunes::class.java)
 
     private var trackList = ArrayList<Track>()
     private var adapter = TrackAdapter()
     private val historyAdapter = TrackAdapter()
     private lateinit var sharedPrefs: SharedPreferences
+
+
     private lateinit var searchHistory: SearchHistory
 
     @SuppressLint("NotifyDataSetChanged")
@@ -61,7 +62,10 @@ class SearchActivity : AppCompatActivity(), SharedPreferences.OnSharedPreference
         findItems()
         recyclerSetting()
         addChangeListeners()
-        addButtonListeners()
+
+        sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        searchHistory = SearchHistory(sharedPrefs)
+
         sharedPrefs.registerOnSharedPreferenceChangeListener(this)
 
         // Кнопка очищения истории поиска
@@ -72,8 +76,8 @@ class SearchActivity : AppCompatActivity(), SharedPreferences.OnSharedPreference
 
         // Реагирует на смену фокуса в EditText
         inputEditText.setOnFocusChangeListener { view, hasFocus ->
-            historySearchGroup.isVisible = hasFocus && inputEditText.text.isEmpty() &&
-                    searchHistory.get().isNotEmpty()
+            historySearchGroup.isVisible =
+                hasFocus && inputEditText.text.isEmpty() && searchHistory.get().isNotEmpty()
         }
 
         // Реагирует на ввод текста в EditText
@@ -83,8 +87,8 @@ class SearchActivity : AppCompatActivity(), SharedPreferences.OnSharedPreference
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 historySearchGroup.visibility =
-                    if (inputEditText.hasFocus() && p0?.isEmpty() == true
-                        && searchHistory.get().isNotEmpty()
+                    if (inputEditText.hasFocus() && p0?.isEmpty() == true && searchHistory.get()
+                            .isNotEmpty()
                     ) View.VISIBLE else View.GONE
                 recycler.visibility = if (inputEditText.hasFocus() && p0?.isEmpty() == true) {
                     trackList.clear()
@@ -107,19 +111,12 @@ class SearchActivity : AppCompatActivity(), SharedPreferences.OnSharedPreference
             searchHistory.add(track)
         }
     }
-
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
         } else {
             View.VISIBLE
         }
-    }
-
-    companion object {
-        const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
-        const val HISTORY_SEARCH = "HISTORY_SEARCH"
-        const val KEY_LIST_TRACKS = "tracks_history"
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -137,119 +134,124 @@ class SearchActivity : AppCompatActivity(), SharedPreferences.OnSharedPreference
     }
 
     private fun search(lastText: String) {
-        itunesService.search(lastText)
-            .enqueue(object : Callback<ItunesResponse> {
-                @SuppressLint("NotifyDataSetChanged")
-                override fun onResponse(
-                    call: Call<ItunesResponse>,
-                    response: Response<ItunesResponse>
-                ) {
-                    when (response.code()) {
-                        200 -> {
-                            if (response.body()?.results?.isNotEmpty() == true) {
-                                trackList.clear()
-                                trackList.addAll(response.body()?.results!!)
-                                adapter.notifyDataSetChanged()
-                                recycler.isVisible = true
-                                linearNothingFound.isVisible = false
-                                linearNoInternet.isVisible = false
-                            } else {
-                                linearNothingFound.isVisible = true
-                                linearNoInternet.isVisible = false
-                                recycler.isVisible = false
-                                historySearchGroup.isVisible = false
-                            }
-                        }
-                        else -> {
-                            linearNoInternet.isVisible = true
+        itunesService.search(lastText).enqueue(object : Callback<ItunesResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(
+                call: Call<ItunesResponse>, response: Response<ItunesResponse>
+            ) {
+                when (response.code()) {
+                    200 -> {
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            trackList.clear()
+                            trackList.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                            recycler.isVisible = true
                             linearNothingFound.isVisible = false
+                            linearNoInternet.isVisible = false
+                        } else {
+                            linearNothingFound.isVisible = true
+                            linearNoInternet.isVisible = false
                             recycler.isVisible = false
                             historySearchGroup.isVisible = false
                         }
                     }
+
+                    else -> {
+                        linearNoInternet.isVisible = true
+                        linearNothingFound.isVisible = false
+                        recycler.isVisible = false
+                        historySearchGroup.isVisible = false
+                    }
                 }
-                    override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
+            }
+
+            override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
                 linearNoInternet.visibility = View.VISIBLE
                 linearNothingFound.visibility = View.GONE
                 recycler.visibility = View.GONE
             }
-    })
-}
-
-@SuppressLint("RestrictedApi", "NotifyDataSetChanged")
-private fun addButtonListeners() {
-    // Кнопка обновить запрос, когда нет сети
-    updateButton.setOnClickListener {
-        search(lastRequest)
+        })
     }
-    // Кнопка назад
-    returnItemImageView.setOnClickListener {
-        finish()
-    }
-    // Кнопка очистить поле ввода
-    clearButton.setOnClickListener {
-        inputEditText.setText("")
-        hideKeyboard(currentFocus ?: View(this))
-        trackList.clear()
-        adapter.notifyDataSetChanged()
-        linearNothingFound.isVisible = false
-        linearNoInternet.isVisible = false
 
-    }
-}
-
-private fun addChangeListeners() {
-    // Реагирует на нажатие кнопки на клавиатуре и выполняет поиск
-    inputEditText.setOnEditorActionListener { _, actionId, _ ->
-        if (actionId == EditorInfo.IME_ACTION_DONE) {
-            search(lastText = inputEditText.text.toString())
-            lastRequest = inputEditText.text.toString()
-            true
+    @SuppressLint("RestrictedApi", "NotifyDataSetChanged")
+    private fun addButtonListeners() {
+        // Кнопка обновить запрос, когда нет сети
+        updateButton.setOnClickListener {
+            search(lastRequest)
         }
-        false
-    }
-
-    val simpleTextWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        // Кнопка назад
+        returnItemImageView.setOnClickListener {
+            finish()
         }
+        // Кнопка очистить поле ввода
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            hideKeyboard(currentFocus ?: View(this))
+            trackList.clear()
+            adapter.notifyDataSetChanged()
+            linearNothingFound.isVisible = false
+            linearNoInternet.isVisible = false
 
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            clearButton.visibility = clearButtonVisibility(s)
-            inputSaveText = s.toString()
-        }
-
-        override fun afterTextChanged(s: Editable?) {
         }
     }
-    // Реагирует на ввод текста в EditText для отображения кнопок
-    inputEditText.addTextChangedListener(simpleTextWatcher)
-}
 
-private fun findItems() {
-    linearNothingFound = findViewById(R.id.error_block_nothing_found)
-    linearNoInternet = findViewById(R.id.error_block_setting)
-    clearButton = findViewById<Button>(R.id.exit)
-    updateButton = findViewById(R.id.button_update)
-    returnItemImageView = findViewById<ImageView>(R.id.return_n)
-    cleanHistoryButton = findViewById(R.id.clean_history_button)
-    placeholderMessage = findViewById(R.id.placeholderMessage)
-    inputEditText = findViewById(R.id.search_content)
-    historySearchGroup = findViewById(R.id.history_search_group)
-    recycler = findViewById<RecyclerView>(R.id.recyclerView)
-    recyclerViewHistory = findViewById<RecyclerView>(R.id.recycler_view_history)
-}
+    private fun addChangeListeners() {
+        // Реагирует на нажатие кнопки на клавиатуре и выполняет поиск
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search(lastText = inputEditText.text.toString())
+                lastRequest = inputEditText.text.toString()
+                true
+            }
+            false
+        }
 
-private fun recyclerSetting() {
-    recycler.layoutManager = LinearLayoutManager(this)
-    recycler.adapter = adapter
-    recyclerViewHistory.layoutManager = LinearLayoutManager(this)
-    recyclerViewHistory.adapter = historyAdapter
-}
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
 
-override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-    if (key == KEY_LIST_TRACKS) {
-        historyAdapter.trackList = searchHistory.get().toCollection(ArrayList())
-        historyAdapter.notifyDataSetChanged()
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                inputSaveText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        // Реагирует на ввод текста в EditText для отображения кнопок
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
-}
+
+    private fun findItems() {
+        linearNothingFound = findViewById(R.id.error_block_nothing_found)
+        linearNoInternet = findViewById(R.id.error_block_setting)
+        clearButton = findViewById<Button>(R.id.exit)
+        updateButton = findViewById(R.id.button_update)
+        returnItemImageView = findViewById<ImageView>(R.id.return_n)
+        cleanHistoryButton = findViewById(R.id.clean_history_button)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        inputEditText = findViewById(R.id.search_content)
+        historySearchGroup = findViewById(R.id.history_search_group)
+        recycler = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerViewHistory = findViewById<RecyclerView>(R.id.recycler_view_history)
+    }
+
+    private fun recyclerSetting() {
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
+        recyclerViewHistory.layoutManager = LinearLayoutManager(this)
+        recyclerViewHistory.adapter = historyAdapter
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == KEY_LIST_TRACKS) {
+            historyAdapter.trackList = searchHistory.get().toCollection(ArrayList())
+            historyAdapter.notifyDataSetChanged()
+        }
+    }
+    companion object {
+        const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
+        const val HISTORY_SEARCH = "HISTORY_SEARCH"
+        const val KEY_LIST_TRACKS = "tracks_history"
+    }
 }
